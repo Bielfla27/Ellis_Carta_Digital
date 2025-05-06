ec06d5fa866936001cb9cc2ce46b3f7978d857d9
Gabriel Felix <53801722+Bielfla27@users.noreply.github.com>
2025-05-05 22:52:13 -0300

    carta Elis

---
+++ script.js
@@ -0,0 +1,20 @@
+function digitarTexto(elementoId, texto, velocidade = 100) {
+    const elemento = document.getElementById(elementoId);
+    let index = 0;
+  
+    function digitar() {
+      if (index < texto.length) {
+        elemento.innerHTML += texto.charAt(index);
+        index++;
+        setTimeout(digitar, velocidade);
+      }
+    }
+  
+    digitar();
+  }
+  
+  // Exemplo de uso:
+  document.addEventListener("DOMContentLoaded", () => {
+    digitarTexto("textoDigitado", "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.  Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.", 50); // 80ms por letra
+  });
+  
